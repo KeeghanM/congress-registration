626376efba83462366d99e6934c00998fd3b1e3e
Keeghan McGarry <keeghan_m@live.com>
2022-02-23 17:55:20 +0000

    Sign In and Out

--- src/main.js
+++ src/main.js
@@ -1,97 +1,16 @@
 import "animate.css";
-import { firestore, auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import "./scripts/login";
 
-let loginForm = document.querySelector("#login-form");
-let loginFormContainer = document.querySelector("#login-form-container");
-let participantForm = document.querySelector("#participant-form");
-let participantFormContainer = document.querySelector(
-  "#participant-form-container"
-);
+import { auth } from "./scripts/firebase";
+import { signIn, signUserOut } from "./scripts/utils";
+import { onAuthStateChanged } from "firebase/auth";
 
-loginForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = Object.fromEntries(new FormData(e.target));
-  const email = formData["login-email"];
-  const password = formData["login-password"];
-
-  if (password.match("(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}")) {
-    console.log(email);
-  } else {
-    console.log("Password invalid");
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    signIn(user);
   }
-  return;
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-
-      switchPanels(loginFormContainer, participantFormContainer);
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log({ errorCode, errorMessage });
-    });
 });
 
-participantForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  switchPanels(participantFormContainer, loginFormContainer);
+document.querySelector("#signout-btn").addEventListener("click", (e) => {
+  signUserOut();
 });
-
-function switchPanels(panelOut, panelIn) {
-  panelOut.hidden = true;
-  panelIn.hidden = false;
-}
-
-var myInput = document.getElementById("login-password");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
-
-myInput.onfocus = function () {
-  document.getElementById("passwordMessage").style.display = "block";
-};
-
-myInput.onblur = function () {
-  document.getElementById("passwordMessage").style.display = "none";
-};
-
-myInput.onkeyup = function () {
-  var lowerCaseLetters = /[a-z]/g;
-  if (myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-  }
-
-  var upperCaseLetters = /[A-Z]/g;
-  if (myInput.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  var numbers = /[0-9]/g;
-  if (myInput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-
-  if (myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
-};
---
+++ src/scripts/login.js
@@ -0,0 +1,94 @@
+import { auth, firestore } from "./firebase.js";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { signIn, handleError } from "./utils";
+
+// Login form submission
+document.querySelector("#login-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = Object.fromEntries(new FormData(e.target));
+  const email = formData["login-email"];
+  const password = formData["login-password"];
+
+  if (password.match("(?=.*[a-z])(?=.*[A-Z]).{8,}")) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        signIn(userCredential.user);
+      })
+      .catch((error) => {
+        if (error.code == "auth/user-not-found") {
+          createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              let user = userCredential.user;
+              setDoc(doc(firestore, "users", user.uid), {
+                email: user.email,
+              })
+                .then(() => {
+                  signIn(user);
+                })
+                .catch((error) => {
+                  handleError(error.message);
+                });
+            })
+            .catch((error) => {
+              handleError(error.message);
+            });
+        } else {
+          handleError(error.message);
+        }
+      });
+  } else {
+    document.getElementById("passwordMessage").style.display = "block";
+    checkPassword();
+  }
+  return;
+});
+
+// Password Validation Display
+let loginPasswordField = document.getElementById("login-password");
+loginPasswordField.onkeyup = () => checkPassword();
+
+function checkPassword() {
+  let letter = document.getElementById("letter");
+  let capital = document.getElementById("capital");
+  let number = document.getElementById("number");
+  let length = document.getElementById("length");
+  var lowerCaseLetters = /[a-z]/g;
+  var upperCaseLetters = /[A-Z]/g;
+  var numbers = /[0-9]/g;
+
+  if (loginPasswordField.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+
+  if (loginPasswordField.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  if (loginPasswordField.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  if (loginPasswordField.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
