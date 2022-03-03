fee688ff434699ee809447b79da63b75e2ee65fb
Keeghan McGarry <keeghan_m@live.com>
2022-03-03 18:44:28 +0000

    AUth Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { UserContext } from "../lib/context"
+import { useUserData } from "../lib/hooks"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const userData = useUserData()
+
+  return (
+    <UserContext.Provider value={userData}>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </UserContext.Provider>
+  )
 }
 
 export default MyApp
